435b3dbf2dfb38794079eb0d4add37273035ff5b
DanielinLove <cmsg_hbg2@naver.com>
2024-11-11 00:14:48 +0900

    feat: Selena files

---
+++ Python/library.py
@@ -0,0 +1,39 @@
+# 1. 넘파이 가이드 : https://numpy.org/doc/stable/user/absolute_beginners.html
+# 설치방법 : pip install numpy
+# 사용방법 - import numpy as np
+
+# 2. 판다스 가이드 : https://pandas.pydata.org  
+# 판다스 콘다에서 설치 : conda install -c conda-forge pandas
+# 설치방법 : pip install pandas
+# 사용방법 - import pandas as 
+
+# 3. 맷플롯립 가이드 : https://matplotlib.org
+# 용도 : 파이썬에서 데이터를 차트나 플롯으로 시각화
+# matplotlib.pyplot 모듈 이용
+# 설치방법 : pip install matplotlib 
+# 사용방법 - import matplotlib.pyplot as plt
+
+# 4. 씨본 가이드 : https://seaborn.pydata.org : matplotlib 기반의 시각화 라이브러리
+# 용도 : 통계 기반 그래픽을 그리기 위한 고급 인터페이스를 제공
+# 설치방법 : pip install seaborn
+# 사용방법 : import seaborn as sns
+# Swarm Plot : 등급, 나이, 남녀 - 세 가지를 분포로 보여준다. 
+
+# 5. BeautifulSoup 가이드 : https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# 용도 : HTML과 XML 데이터를 쉽게 처리하는 파이썬 라이브러리
+# 설치방법 : pip install beautifulsoup4
+# 사용방법 : from bs4 import BeautifulSoup
+
+# 6. Scikit Learn : https://scikit-learn.org/stable/getting_started.html
+# Scikit Learn : https://scikit-learn.org/stable/index.html 
+# 용도 : R에 대응하는 Python 계의 ML 분석 라이브러리. ML은 곧 Scikit-Learn을 의미할 정도임
+# 설치방법 : pip install scikit-learn
+# 사용방법 : import scikit-learn
+
+
+
+
+
+
+import library as np
+import pandas as pd
---
+++ Python/실습용 Colab 파일/test.py
@@ -0,0 +1,13 @@
+# %%
+import pandas as pd
+
+# CSV 파일 불러오기
+df = pd.read_csv('titanic.csv')
+
+# 모든 행과 열을 표시하도록 설정
+pd.set_option('display.max_rows', None)  # 모든 행 표시
+pd.set_option('display.max_columns', None)  # 모든 열 표시
+
+# 데이터 출력
+display(df)  # Jupyter Notebook에서 전체 테이블을 표시
+# %%
