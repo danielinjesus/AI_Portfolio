26caf9bcd4f13aa52feee7bdf97dadb0a01dddc7
DanielinLove <cmsg_hbg2@naver.com>
2024-11-11 13:57:48 +0000

    feat: null x,y

--- Python/실습용 Colab 파일/test.py
+++ Python/실습용 Colab 파일/test.py
@@ -11,3 +11,14 @@ pd.set_option('display.max_columns', None)  # 모든 열 표시
 # 데이터 출력
 display(df)  # Jupyter Notebook에서 전체 테이블을 표시
 # %%
+import pandas as pd
+df = pd.read_csv('/data/ephemeral/home/train.csv')
+nullcnt = df.isnull().sum()
+notnullcnt = df.notnull().sum()
+nullnot = pd.concat([nullcnt + notnullcnt], axis=0)
+result_df = pd.DataFrame({
+    'null_count': nullcnt,
+    'notnull_count': notnullcnt,
+    'full_number' : nullnot })
+display(result_df)
+# %%
