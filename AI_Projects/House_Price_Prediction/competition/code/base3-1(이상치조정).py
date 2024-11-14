ffbed8cc0473b7574d1e7cf152e8c1e608504b85
DanielinLove <cmsg_hbg2@naver.com>
2024-11-14 07:04:41 +0000

    log_trans

---
+++ AI_Projects/House_Price_Prediction/competition/code/base3-1(이상치조정).py
@@ -0,0 +1,454 @@
+# %%
+ # 변화점
+# 1. baseline 코드 오타 수정
+# 2. baseline 코드 .py로 변환
+# 3. 시작 부분 get_ipython() 두 줄 주석화
+# 4. iPython display 부분 import
+# 5. 마지막에 # %%time 제거. (ipynb에서만 작동하는 매직 명령어)
+
+from IPython.display import display
+# get_ipython().system('pip install eli5==0.13.0')
+
+# # 한글 폰트 사용을 위한 라이브러리입니다.
+# get_ipython().system('apt-get install -y fonts-nanum')
+
+# visualization
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+fe = fm.FontEntry(
+    fname=r'/usr/share/fonts/truetype/nanum/NanumGothic.ttf', # ttf 파일이 저장되어 있는 경로
+    name='NanumBarunGothic')                        # 이 폰트의 원하는 이름 설정
+fm.fontManager.ttflist.insert(0, fe)              # Matplotlib에 폰트 추가
+plt.rcParams.update({'font.size': 10, 'font.family': 'NanumBarunGothic'}) # 폰트 설정
+plt.rc('font', family='NanumBarunGothic')
+import seaborn as sns
+
+# utils
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import pickle
+import warnings;warnings.filterwarnings('ignore')
+
+# Model
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+# 필요한 데이터를 load 하겠습니다. 경로는 환경에 맞게 지정해주면 됩니다.
+train_path = '/data/ephemeral/home/train.csv' 
+test_path  = '/data/ephemeral/home/AI_Portfolio/AI_Projects/House_Price_Prediction/competition/data/test.csv'
+dt = pd.read_csv(train_path)
+dt_test = pd.read_csv(test_path)
+
+dt.info()
+
+# Train data와 Test data shape은 아래와 같습니다.
+print('Train data shape : ', dt.shape, 'Test data shape : ', dt_test.shape)
+
+# Train과 Test data를 살펴보겠습니다.
+display(dt.head(1))
+display(dt_test.head(1))      # 부동산 실거래가(=Target) column이 제외된 모습입니다.
+
+dt.columns
+
+# ## 3. Data Preprocessing
+
+# - 모델링 전에 데이터 내 결측치, 이상치 등을 제거하고 범주형과 연속형 변수를 살펴보도록 하겠습니다!
+# - 먼저, 용이한 전처리를 위해 train과 test data를 합친 하나의 데이터로 진행하도록 하겠습니다.
+
+# train/test 구분을 위한 칼럼을 하나 만들어 줍니다.
+dt['is_test'] = 0
+dt_test['is_test'] = 1
+concat = pd.concat([dt, dt_test])     # 하나의 데이터로 만들어줍니다.
+print(concat)
+
+concat['is_test'].value_counts()      # train과 test data가 하나로 합쳐진 것을 확인할 수 있습니다.
+
+# 칼럼 이름을 쉽게 바꿔주겠습니다. 다른 칼럼도 사용에 따라 바꿔주셔도 됩니다!
+concat = concat.rename(columns={'전용면적(㎡)':'전용면적'})
+print(concat)
+
+# ### 3.1. 결측치 탐색 및 보간
+# - 결측치를 처리할 때 많이 쓰는 방법은 삭제(Deletion)과 대체(Imputation) 입니다.
+# - 본 Baseline에서는 결측치가 90% 이상을 차지하는 변수의 결측치는 제거하고, 나머지 결측치는 대체하도록 하겠습니다.
+#   - 연속형 변수의 경우에는 선형보간(Linear interpolation)으로 대체하겠습니다.
+#   - 범주형 변수의 경우에는 'NULL' 이라는 임의의 범주로 대체해주도록 하겠습니다.
+
+# - 참고 ✅
+#   - 선형보간(Linear interpolation)이란 두 점의 값이 주어졌을 때 **그 사이의 값**을 추정하기 위해 직선 거리에 따라 선형적으로 계산하는 방법입니다.
+#   - 아래 두개의 붉은 점 사이에 ($x$, $y$) 값을 추정하기 위해, 해당 점이 두 붉은 점을 이은 선형적인 선분에 있다고 가정하고, 단순 비례식으로 ($x$, $y$)를 추정해 보간하는 방법입니다.
+#   - [참고자료](https://ko.wikipedia.org/wiki/%EC%84%A0%ED%98%95_%EB%B3%B4%EA%B0%84%EB%B2%95#:~:text=%EC%84%A0%ED%98%95%20%EB%B3%B4%EA%B0%84%EB%B2%95(%E7%B7%9A%E5%9E%8B%E8%A3%9C%E9%96%93%E6%B3%95,%EC%9C%BC%EB%A1%9C%20%EA%B3%84%EC%82%B0%ED%95%98%EB%8A%94%20%EB%B0%A9%EB%B2%95%EC%9D%B4%EB%8B%A4.)
+
+# 실제로 결측치라고 표시는 안되어있지만 아무 의미도 갖지 않는 element들이 아래와 같이 존재합니다.
+# 아래 3가지의 경우 모두 아무 의미도 갖지 않는 element가 포함되어 있습니다.
+print(concat['등기신청일자'].value_counts())
+
+print(concat['거래유형'].value_counts())
+
+print(concat['중개사소재지'].value_counts())
+
+# 위 처럼 아무 의미도 갖지 않는 칼럼은 결측치와 같은 역할을 하므로, np.nan으로 채워 결측치로 인식되도록 합니다.
+concat['등기신청일자'] = concat['등기신청일자'].replace(' ', np.nan)
+concat['거래유형'] = concat['거래유형'].replace('-', np.nan)
+concat['중개사소재지'] = concat['중개사소재지'].replace('-', np.nan)
+
+# EDA에 앞서 결측치를 확인해보겠습니다.
+concat.isnull().sum()
+
+# 변수별 결측치의 비율을 plot으로 그려보면 아래와 같습니다.
+fig = plt.figure(figsize=(13, 2))
+missing = concat.isnull().sum() / concat.shape[0]
+missing = missing[missing > 0]
+missing.sort_values(inplace=True)
+missing.plot.bar(color='orange')
+plt.title('변수별 결측치 비율')
+plt.show()
+
+# - 위 그래프에서 볼 수 있다시피 결측치가 100만개 이상인 칼럼들은 결측치가 해당 칼럼의 element 중 90% 이상을 차지하는 상황이 됩니다.
+# - 따라서 본 Baseline 에서는 이 칼럼은 예측에 도움이 되지 않는다고 판단해 삭제해주도록 하겠습니다.
+
+# Null값이 100만개 이상인 칼럼은 삭제해보도록 하겠습니다.
+print('* 결측치가 100만개 이하인 변수들 :', list(concat.columns[concat.isnull().sum() <= 1000000]))     # 남겨질 변수들은 아래와 같습니다.
+print('* 결측치가 100만개 이상인 변수들 :', list(concat.columns[concat.isnull().sum() >= 1000000]))
+
+# 위에서 결측치가 100만개 이하인 변수들만 골라 새로운 concat_select 객체로 저장해줍니다.
+selected = list(concat.columns[concat.isnull().sum() <= 1000000])
+concat_select = concat[selected]
+len(concat_select.columns)
+
+concat_select.isnull().sum()     # 결측치가 100만개 초과인 칼럼이 제거된 모습은 아래와 같습니다.
+# target변수는 test dataset 개수만큼(9272) 결측치가 존재함을 확인할 수 있습니다.
+
+# 연속형 변수는 선형보간을 해주고, 범주형변수는 알수없기에 “unknown”이라고 임의로 보간해 주겠습니다.
+concat_select.info()
+
+# 본번, 부번의 경우 float로 되어있지만 범주형 변수의 의미를 가지므로 object(string) 형태로 바꾸어주고 아래 작업을 진행하겠습니다.
+concat_select['본번'] = concat_select['본번'].astype('str')
+concat_select['부번'] = concat_select['부번'].astype('str')
+
+# 먼저, 연속형 변수와 범주형 변수를 위 info에 따라 분리해주겠습니다.
+continuous_columns = []
+categorical_columns = []
+
+for column in concat_select.columns:
+    if pd.api.types.is_numeric_dtype(concat_select[column]):
+        continuous_columns.append(column)
+    else:
+        categorical_columns.append(column)
+
+print("연속형 변수:", len(continuous_columns), continuous_columns)
+print("범주형 변수:", len(categorical_columns), categorical_columns)
+
+# - 연속형 변수와 범주형 변수가 알맞게 나누어졌는지 확인해보고, 이제 각각 보간을 진행합니다.
+# 범주형 변수에 대한 보간
+concat_select[categorical_columns] = concat_select[categorical_columns].fillna('NULL')
+
+# 연속형 변수에 대한 보간 (선형 보간)
+concat_select[continuous_columns] = concat_select[continuous_columns].interpolate(method='linear', axis=0)
+
+concat_select.isnull().sum()         # 결측치가 보간된 모습을 확인해봅니다.
+
+# - Baseline에서는 단순 결측치들을 제거하거나 보간했지만,
+# EDA를 통해 변수들의 흐름을 조금 더 파악하고 더 나은 interpolation 등의 방법을 고려해볼 수 있을 것 같습니다.
+
+# ### 3.2. 이상치 처리
+# - 이번엔 이상치를 처리해봅시다.
+# - 이상치를 처리하는 방법은 IQR, Z-score 등 다양한 방법이 있습니다. 가장 쉽고 직관적으로 접근하는 방법은 기본 통계치를 이용하는 것입니다.
+# - 본 baseline에서는 IQR를 이용한 방법을 사용하겠습니다.
+
+# 이상치 제거 이전의 shape은 아래와 같습니다.
+print(concat_select.shape)
+
+# 대표적인 연속형 변수인 “전용 면적” 변수 관련한 분포를 먼저 살펴보도록 하겠습니다.
+fig = plt.figure(figsize=(7, 3))
+sns.boxplot(data = concat_select, x = '전용면적', color='lightgreen')
+plt.title('전용면적 분포')
+plt.xlabel('Area')
+plt.show()
+
+# # 이상치 제거 방법에는 IQR을 이용하겠습니다.
+# def remove_outliers_iqr(dt, column_name):
+#     df = dt.query('is_test == 0')       # train data 내에 있는 이상치만 제거하도록 하겠습니다.
+#     df_test = dt.query('is_test == 1')
+
+#     Q1 = df[column_name].quantile(0.25)
+#     Q3 = df[column_name].quantile(0.75)
+#     IQR = Q3 - Q1
+
+#     lower_bound = Q1 - 1.5 * IQR
+#     upper_bound = Q3 + 1.5 * IQR
+
+#     df = df[(df[column_name] >= lower_bound) & (df[column_name] <= upper_bound)]
+
+#     result = pd.concat([df, df_test])   # test data와 다시 합쳐주겠습니다.
+#     return result
+
+# # 위 방법으로 전용 면적에 대한 이상치를 제거해보겠습니다.
+# concat_select = remove_outliers_iqr(concat_select, '전용면적')
+
+# # 이상치 제거 후의 shape은 아래와 같습니다. 약 10만개의 데이터가 제거된 모습을 확인할 수 있습니다.
+# print(concat_select.shape)
+
+concat_select['is_test'].value_counts()     # 또한, train data만 제거되었습니다.
+
+# ## 4. Feature Engineering
+# - 이제 파생변수를 만들어보도록 하겠습니다.
+# - 파생 변수는 도메인 지식에 기반해 제작하는 것이 중요합니다.
+# - 주택의 가격을 예측하는 문제이기 때문에 약간의 부동산 지식을 기반으로 파생 변수를 만들어 보도록 하겠습니다.
+
+# 시군구, 년월 등 분할할 수 있는 변수들은 세부사항 고려를 용이하게 하기 위해 모두 분할해 주겠습니다.
+concat_select['구'] = concat_select['시군구'].map(lambda x : x.split()[1])
+concat_select['동'] = concat_select['시군구'].map(lambda x : x.split()[2])
+del concat_select['시군구']
+
+concat_select['계약년'] = concat_select['계약년월'].astype('str').map(lambda x : x[:4])
+concat_select['계약월'] = concat_select['계약년월'].astype('str').map(lambda x : x[4:])
+del concat_select['계약년월']
+
+concat_select.columns
+
+# - 서울의 집값은 강남, 강북 여부에 따라 차이가 많이 난다는 사실은 많이 알려진 사실입니다.
+# - 따라서 강남/강북의 여부에 따라 파생변수를 생성해주도록 하겠습니다.
+
+all = list(concat_select['구'].unique())
+gangnam = ['강서구', '영등포구', '동작구', '서초구', '강남구', '송파구', '강동구']
+gangbuk = [x for x in all if x not in gangnam]
+
+assert len(all) == len(gangnam) + len(gangbuk)       # 알맞게 분리되었는지 체크합니다.
+
+# 강남의 여부를 체크합니다.
+is_gangnam = []
+for x in concat_select['구'].tolist() :
+  if x in gangnam :
+    is_gangnam.append(1)
+  else :
+    is_gangnam.append(0)
+
+# 파생변수를 하나 만릅니다.
+concat_select['강남여부'] = is_gangnam
+
+print(concat_select.columns)
+
+len(concat_select.columns)
+
+# - 또한 신축인지, 구축인지의 여부도 실거래가에 큰 영향을 줄 수 있습니다.
+# - 따라서 건축년도에 따라 파생변수를 제작해주도록 하겠습니다.
+
+# 건축년도 분포는 아래와 같습니다. 특히 2005년이 Q3에 해당합니다.
+# 2009년 이후에 지어진 건물은 10%정도 되는 것을 확인할 수 있습니다.
+concat_select['건축년도'].describe(percentiles = [0.1, 0.25, 0.5, 0.75, 0.8, 0.9])
+
+# 따라서 2009년 이후에 지어졌으면 비교적 신축이라고 판단하고, 신축 여부 변수를 제작해보도록 하겠습니다.
+concat_select['신축여부'] = concat_select['건축년도'].apply(lambda x: 1 if x >= 2009 else 0)
+concat_select.head(1)       # 최종 데이터셋은 아래와 같습니다.
+concat_select.shape
+
+# - 위 두 사항 외에도 외부 공공 데이터를 이용하거나, EDA를 통해 더 많은 변수를 제작할 수 있습니다.
+
+# ## 5. Model Training
+
+# - 이제 위에서 만든 파생변수들과 정제한 데이터를 기반으로 본격적으로 부동산 실거래가를 예측하는 모델링을 진행하겠습니다.
+# - 모델링에는 `sklearn`의 `RandomForest`를 이용하도록 하겠습니다.
+# 
+# - 참고 ✅
+#   - `RandomForest`는 배깅(Bagging)의 일종으로, 학습시키는 데이터 뿐 아니라 특성변수(X)들도 무작위로 선택해 트리를 생성하는 방법입니다.
+#   - 모델 학습 과정에서 서로 다른 N개의 Tree 생성하고, N개의 Tree에서 발생한 Output을 Voting(범주형, 분류문제)하거나, Average(연속형, 회귀문제)해 최종 Output 생성합니다.
+#   - 이는 High variance, Low bias 상황에서 분산(Variance) 감소에 도움을 줍니다.
+
+# 이제 다시 train과 test dataset을 분할해줍니다. 위에서 제작해 놓았던 is_test 칼럼을 이용합니다.
+dt_train = concat_select.query('is_test==0')
+dt_test = concat_select.query('is_test==1')
+
+# 이제 is_test 칼럼은 drop해줍니다.
+dt_train.drop(['is_test'], axis = 1, inplace=True)
+dt_test.drop(['is_test'], axis = 1, inplace=True)
+print(dt_train.shape, dt_test.shape)
+
+dt_test.head(1)
+
+# dt_test의 target은 일단 0으로 임의로 채워주도록 하겠습니다.
+dt_test['target'] = 0
+
+# ### 5.1. 범주형 변수 Encoding
+# - 범주형 변수는 그대로 모델에 투입하면, 모델이 제대로 작동할 수 없습니다.
+# - 따라서 **레이블 인코딩 과정**을 통해 범주형 변수들을 numeric하게 바꾸는 인코딩 과정을 진행해주도록 하겠습니다.
+# 파생변수 제작으로 추가된 변수들이 존재하기에, 다시한번 연속형과 범주형 칼럼을 분리해주겠습니다.
+continuous_columns_v2 = []
+categorical_columns_v2 = []
+
+for column in dt_train.columns:
+    if pd.api.types.is_numeric_dtype(dt_train[column]):
+        continuous_columns_v2.append(column)
+    else:
+        categorical_columns_v2.append(column)
+
+print("연속형 변수:", continuous_columns_v2)
+print("범주형 변수:", categorical_columns_v2)
+
+# 아래에서 범주형 변수들을 대상으로 레이블인코딩을 진행해 주겠습니다.
+
+# 각 변수에 대한 LabelEncoder를 저장할 딕셔너리
+label_encoders = {}
+
+# Implement Label Encoding
+for col in tqdm( categorical_columns_v2 ):
+    lbl = LabelEncoder()
+
+    # Label-Encoding을 fit
+    lbl.fit( dt_train[col].astype(str) )
+    dt_train[col] = lbl.transform(dt_train[col].astype(str))
+    label_encoders[col] = lbl           # 나중에 후처리를 위해 레이블인코더를 저장해주겠습니다.
+
+    # Test 데이터에만 존재하는 새로 출현한 데이터를 신규 클래스로 추가해줍니다.
+    for label in np.unique(dt_test[col]):
+      if label not in lbl.classes_: # unseen label 데이터인 경우
+        lbl.classes_ = np.append(lbl.classes_, label) # 미처리 시 ValueError발생하니 주의하세요!
+
+    dt_test[col] = lbl.transform(dt_test[col].astype(str))
+
+dt_train.head(1)        # 레이블인코딩이 된 모습입니다.
+
+# ### 5.2. Model Training
+# - 위 데이터를 이용해 모델을 train 해보겠습니다. 모델은 RandomForest를 이용하겠습니다.
+# - Train과 Valid dataset을 분할하는 과정에서는 `holdout` 방법을 사용하겠습니다. 이 방법의 경우  대략적인 성능을 빠르게 확인할 수 있다는 점에서 baseline에서 사용해보도록 하겠습니다.
+#   - 이 후 추가적인 eda를 통해서 평가세트와 경향을 맞추거나 kfold와 같은 분포에 대한 고려를 추가할 수 있습니다.
+
+assert dt_train.shape[1] == dt_test.shape[1]          # train/test dataset의 shape이 같은지 확인해주겠습니다.
+
+# Target과 독립변수들을 분리해줍니다.
+y_train = dt_train['target']
+X_train = dt_train.drop(['target'], axis=1)
+
+# Hold out split을 사용해 학습 데이터와 검증 데이터를 8:2 비율로 나누겠습니다.
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=2023)
+
+# RandomForestRegressor를 이용해 회귀 모델을 적합시키겠습니다.
+model = RandomForestRegressor(n_estimators=5, criterion='squared_error', random_state=1, n_jobs=-1)
+model.fit(X_train, y_train)
+pred = model.predict(X_val)
+
+# - 랜덤포레스트의 하이퍼파라미터도 데이터에 맞게 지정해줄 수 있습니다. 데이터에 맞는 하이퍼파라미터를 찾는 것도 성능 향상에 도움이 될 수 있습니다.
+
+# 회귀 관련 metric을 통해 train/valid의 모델 적합 결과를 관찰합니다.
+print(f'RMSE test: {np.sqrt(metrics.mean_squared_error(y_val, pred))}')
+# %%
+# - 변수 중요도도 확인해보도록 하겠습니다.
+
+# 위 feature importance를 시각화해봅니다.
+importances = pd.Series(model.feature_importances_, index=list(X_train.columns))
+importances = importances.sort_values(ascending=False)
+
+plt.figure(figsize=(10,8))
+plt.title("Feature Importances")
+sns.barplot(x=importances, y=importances.index)
+plt.show()
+
+# 학습된 모델을 저장합니다. Pickle 라이브러리를 이용하겠습니다.
+location = '/data/ephemeral/home/model-pkl/'
+file_name = 'base3_model'
+location_file_name = location + file_name + '.pkl'
+with open(f'{location_file_name}', 'wb') as f:
+    pickle.dump(model, f)
+
+# ### 5.3. Feature selection
+
+# - 어떤 변수가 유용한 변수인지 확인해보기 위해 permutation selection을 이용해보겠습니다.
+
+# Permutation importance 방법을 변수 선택에 이용해보겠습니다.
+perm = PermutationImportance(model,        # 위에서 학습된 모델을 이용하겠습니다.
+                             scoring = "neg_mean_squared_error",        # 평가 지표로는 회귀문제이기에 negative rmse를 사용합니다. (neg_mean_squared_error : 음의 평균 제곱 오차)
+                             random_state = 42,
+                             n_iter=3).fit(X_val, y_val)
+eli5.show_weights(perm, feature_names = X_val.columns.tolist())    # valid data에 대해 적합시킵니다.
+
+# - 분석 결과 "계약년", "전용면적" 변수가 유의한 변수로 보입니다. 따라서 이 변수로 또 다른 파생변수를 더 생성해보거나, 중요도가 낮아보이는 변수를 제거해 차원의 저주를 막아볼 수도 있습니다.
+# ### 5.4. Valid prediction 분석
+
+# - 예측값을 분석해보기 위해 valid prediction을 확인해보겠습니다.
+
+# Validation dataset에 target과 pred 값을 채워주도록 하겠습니다.
+X_val['target'] = y_val
+X_val['pred'] = pred
+
+# Squared_error를 계산하는 함수를 정의하겠습니다.
+def calculate_se(target, pred):
+    squared_errors = (target - pred) ** 2
+    return squared_errors
+
+# RMSE 계산
+squared_errors = calculate_se(X_val['target'], X_val['pred'])
+X_val['error'] = squared_errors
+
+# Error가 큰 순서대로 sorting 해 보겠습니다.
+X_val_sort = X_val.sort_values(by='error', ascending=False)       # 내림차순 sorting
+
+X_val_sort.head()
+
+# - 예측을 잘 하지 못한 top 100개의 데이터와 예측을 잘한 top 100개의 데이터를 비교해보겠습니다.
+
+X_val_sort_top100 = X_val.sort_values(by='error', ascending=False).head(100)        # 예측을 잘 하지못한 top 100개의 data
+X_val_sort_tail100 = X_val.sort_values(by='error', ascending=False).tail(100)       # 예측을 잘한 top 100개의 data
+
+# 해석을 위해 레이블인코딩 된 변수를 복원해줍니다.
+error_top100 = X_val_sort_top100.copy()
+for column in categorical_columns_v2 :     # 앞서 레이블 인코딩에서 정의했던 categorical_columns_v2 범주형 변수 리스트를 사용합니다.
+    error_top100[column] = label_encoders[column].inverse_transform(X_val_sort_top100[column])
+
+best_top100 = X_val_sort_tail100.copy()
+for column in categorical_columns_v2 :     # 앞서 레이블 인코딩에서 정의했던 categorical_columns_v2 범주형 변수 리스트를 사용합니다.
+    best_top100[column] = label_encoders[column].inverse_transform(X_val_sort_tail100[column])
+
+print(error_top100.head(1))
+print(best_top100.head(1))
+
+# - 이제 분포를 비교해보도록 하겠습니다.
+
+sns.boxplot(data = error_top100, x='target')
+plt.title('The worst top100 prediction의 target 분포')
+plt.show()
+
+sns.boxplot(data = best_top100, x='target', color='orange')
+plt.title('The best top100 prediction의 target 분포')
+plt.show()
+
+# - Taget 분포를 보니 좋은 예측을 보인 top 100개의 data보다 상대적으로 나쁜 예측을 보인 top 100 dataset들이 높은 가격을 보였음을 확인할 수 있습니다. 이에 대한 모델링 및 처리가 필요해보입니다.
+
+sns.histplot(data = error_top100, x='전용면적', alpha=0.5)
+sns.histplot(data = best_top100, x='전용면적', color='orange', alpha=0.5)
+plt.title('전용면적 분포 비교')
+plt.show()
+
+# 전용면적 또한 나쁜 예측을 보인 집들이 더 넓음을 확인할 수 있습니다.
+# ## 6. Inference
+
+dt_test.head(2)      # test dataset에 대한 inference를 진행해보겠습니다.
+
+# location = '/data/ephemeral/home/model-pkl/'
+# file_name = 'base3_model'
+# location_file_name = location + file_name + '.pkl'
+# with open(location_file_name, 'wb') as f:
+
+# 저장된 모델을 불러옵니다.
+with open(location_file_name, 'rb') as f:
+    model = pickle.load(f)
+
+X_test = dt_test.drop(['target'], axis=1)
+
+# Test dataset에 대한 inference를 진행합니다.
+real_test_pred = model.predict(X_test)
+
+real_test_pred          # 예측값들이 출력됨을 확인할 수 있습니다.
+
+# ## 7. Output File Save
+
+output = '/data/ephemeral/home/AI_Portfolio/AI_Projects/House_Price_Prediction/competition/submission' + 'file_name'
+# 앞서 예측한 예측값들을 저장합니다.
+preds_df = pd.DataFrame(real_test_pred.astype(int), columns=["target"])
+preds_df.to_csv(f'{output}_output.csv', index=False)
+# %%
